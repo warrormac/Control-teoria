6d72ad13d298ebe13f49364db404e71973156240
andre <warrormac@gmail.com>
2018-05-02 17:02:25 -0700

    retorna 1 si la subcadena esta en la cadena principal

---
+++ subcadena.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int comp(char x[],char y[],int a,int b)
+{
+    int c=0,d=0,e=0;
+    while (c<=a)
+    {
+        if (x[c]==y[d])
+        {
+            d+=1;
+            if (d==b)
+                return 1;
+
+        }
+        c++;
+    }
+    return 0;
+}
+
+int main()
+{
+    char a[]={"hola mundo"};
+    char b[]={"mun"};
+    int tam1,tam2,fin;
+    fin=0;
+    tam1=sizeof(a)-1;
+    tam2=sizeof(b)-1;
+
+    fin=comp(a,b,tam1,tam2);
+    cout<<fin;
+}
